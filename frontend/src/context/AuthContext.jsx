eea806b677e354db322467320110c5f545deab06
Kunal Vishwakarma <kunalvishwakarma7185@gmail.com>
2025-12-02 18:51:08 +0530

    Fix server URL in AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react'
 export const authDataContext=createContext()
 function AuthContext({children}) {
-const serverUrl="https://linkedin-backends.onrender.com"
+const serverUrl="https://linkedin-backendss.onrender.com"
     let value={
         serverUrl
     }
